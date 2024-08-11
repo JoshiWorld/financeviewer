da7b0cd3cced2e5a25eb5c59c8d7ba6889ec517a
Joshua Stieber <joshua.stieber.1903@gmail.com>
2024-08-11 11:45:03 +0200

    fix: removed wrong header link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -16,9 +16,6 @@ export function Navbar() {
           </h3>
         </div>
         <div className="m-2 flex-grow px-4 py-2 text-center">
-          <Link href="/" className="border-b-2 border-primary font-semibold">
-            Finanzen
-          </Link>
         </div>
         <div className="m-2 flex flex-none items-center justify-between px-4 py-2 text-center">
           <Link
